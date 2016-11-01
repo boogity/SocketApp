5e7570f87b09afa7cd0cbf8870390aaae933fbe5
boogity <harumph505@gmail.com>
2016-11-01 12:37:11 -0500

    Change socket code

--- socket.py
+++ socket.py
@@ -2,55 +2,55 @@
 
 #https://www.tutorialspoint.com/python/python_networking.htm
 #Hoping this works
-import socket
+# import socket
 
-s = socket.socket(AF_INET, SOCK_STREAM)			#Creating socket object
-host = socket.gethostname()						#Gets local machine name
-port = 69696									#Reserving port for socket
-s.bind(host, port)								#Binding socket to ip/port
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)			#Creating socket object
+# host = socket.gethostname()						#Gets local machine name
+# port = 69696									#Reserving port for socket
+# s.bind(host, port)								#Binding socket to ip/port
 
-s.listen(5)										#Wait for client connection (5) is default behavior
-while True:
-	c, addr = s.accept()						#Establish connection
-	print 'Connection established with', addr	
-	c.send('You are currently connected')
-	c.close()
+# s.listen(5)										#Wait for client connection (5) is default behavior
+# while True:
+# 	c, addr = s.accept()						#Establish connection
+# 	print 'Connection established with', addr	
+# 	c.send('You are currently connected')
+# 	c.close()
 	
 
 
 
-#https://docs.python.org/2/howto/sockets.html
-#	This tutorial is a bit harder / more complex I think
-# class mysocket:
-#     '''demonstration class only
-#       - coded for clarity, not efficiency
-#     '''
+# https://docs.python.org/2/howto/sockets.html
+# 	This tutorial is a bit harder / more complex I think
+class mysocket:
+    '''demonstration class only
+      - coded for clarity, not efficiency
+    '''
 
-#     def __init__(self, sock=None):
-#         if sock is None:
-#             self.sock = socket.socket(
-#                 socket.AF_INET, socket.SOCK_STREAM)
-#         else:
-#             self.sock = sock
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
 
-#     def connect(self, host, port):
-#         self.sock.connect((host, port))
+    def connect(self, host, port):
+        self.sock.connect((host, port))
 
-#     def mysend(self, msg):
-#         totalsent = 0
-#         while totalsent < MSGLEN:
-#             sent = self.sock.send(msg[totalsent:])
-#             if sent == 0:
-#                 raise RuntimeError("socket connection broken")
-#             totalsent = totalsent + sent
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
 
-#     def myreceive(self):
-#         chunks = []
-#         bytes_recd = 0
-#         while bytes_recd < MSGLEN:
-#             chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
-#             if chunk == '':
-#                 raise RuntimeError("socket connection broken")
-#             chunks.append(chunk)
-#             bytes_recd = bytes_recd + len(chunk)
-#         return ''.join(chunks)
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return ''.join(chunks)
